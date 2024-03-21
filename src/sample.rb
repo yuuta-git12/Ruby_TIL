c8ca358a656b7390968d557d199ce5f33d0a5476
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-03-21 21:57:21 +0900

    [update]ディレクトリ配置修正

--- src/sample.rb
+++ src/sample.rb
@@ -5,3 +5,52 @@ puts a
 
 b = 'こんにちは'
 puts b
+
+n = 1
+# n++
+n += 1
+puts n
+
+n = 1
+n -= 1
+puts n
+
+n = 2
+n *= 2
+puts n
+
+n = 2
+n /= 2
+puts n
+
+n = 2
+n **= 3
+puts n
+
+a = 0.1
+b = 3.0
+puts a.rationalize*b.rationalize
+
+n = 9
+result = if n>10
+  '10より大きい'
+else
+  '10より小さい'
+end
+
+puts result
+
+point = 7
+day = 1
+# 1日であればポイント5倍
+if day == 1
+  point *= 5
+end
+puts point
+
+# 後置ifを使った書き方
+point = 7
+day = 1
+# 1日であればポイント5倍
+point *= 5 if day == 1
+puts point
