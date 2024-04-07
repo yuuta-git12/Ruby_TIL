56d42ce0fe39d0e68981d4965989d112ecfb177a
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-04-07 18:11:09 +0900

    [add]学習に使ったコード

--- src/ruby-book/test/fizz_buzz_test.rb
+++ src/ruby-book/test/fizz_buzz_test.rb
@@ -4,7 +4,17 @@ require_relative '../lib/fizz_buzz' # このファイルのファイルパスを
 
 class FizzBuzzTest < Minitest::Test
      def test_fizz_buzz
-          assert_equal '1',fizz_buzz(1)
+          assert_equal '1',fizz_buzz(テスト),msg='失敗です'
+          assert_equal '2',fizz_buzz(2)
+          assert_equal 'Fizz',fizz_buzz(3)
+          assert_equal '4',fizz_buzz(4)
+          assert_equal 'Buzz',fizz_buzz(5)
+          assert_equal 'Fizz',fizz_buzz(6)
+          assert_equal 'Fizz Buzz',fizz_buzz(15)
+     end
+
+     def test_fizz_buzz2
+          # assert_equal '1',fizz_buzz(10),msg='失敗です'
           assert_equal '2',fizz_buzz(2)
           assert_equal 'Fizz',fizz_buzz(3)
           assert_equal '4',fizz_buzz(4)
