10219c4bfa05d72759d8414076b0e1078618564c
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-06-09 22:14:43 +0900

    [update]規定外のキーワードも受け取れるように変更

--- src/chapter5/5_4.rb
+++ src/chapter5/5_4.rb
@@ -7,11 +7,10 @@ person = {
   phones: { home: '1234-0000', mobile: '5678-0000' }
 }
 
-def buy_burger(menu, options = {})
+def buy_burger(menu, drink: true, potato: true, **others)
   # ハンバーガーを購入
   message = "メニュー:#{menu}"
-  drink = options[:drink]
-  potato = options[:potato]
+
   if drink
     # ドリンクを購入
     message << ' ドリンク付き'
@@ -22,6 +21,8 @@ def buy_burger(menu, options = {})
     message << ' ポテト付き'
   end
   puts message
+  # 指定されていないキーワードを表示する処理
+  puts others
 end
 
 buy_burger('cheese')
@@ -30,6 +31,4 @@ buy_burger('fish', drink: false)
 
 buy_burger('big', potato: true, drink: true)
 
-# 擬似キーワード引数の場合はどんなキーワードを指定してもエラーにならない
-# 無効なキー(ここではchicken)をエラーにするためにはメソッド側で明示的な実装が必要
 buy_burger('big', potato: true, drink: true, chicken: false)
