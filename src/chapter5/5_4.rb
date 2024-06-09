4481998dc9ad59a01fdf0fc1c328dd350c7ab5ce
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-06-09 22:09:27 +0900

    [update]擬似キーワード引数で書き換え

--- src/chapter5/5_4.rb
+++ src/chapter5/5_4.rb
@@ -7,9 +7,11 @@ person = {
   phones: { home: '1234-0000', mobile: '5678-0000' }
 }
 
-def buy_burger(menu, drink: true, potato: true)
+def buy_burger(menu, options = {})
   # ハンバーガーを購入
   message = "メニュー:#{menu}"
+  drink = options[:drink]
+  potato = options[:potato]
   if drink
     # ドリンクを購入
     message << ' ドリンク付き'
@@ -19,7 +21,7 @@ def buy_burger(menu, drink: true, potato: true)
     # ポテトを購入
     message << ' ポテト付き'
   end
-  p message
+  puts message
 end
 
 buy_burger('cheese')
@@ -27,3 +29,7 @@ buy_burger('cheese')
 buy_burger('fish', drink: false)
 
 buy_burger('big', potato: true, drink: true)
+
+# 擬似キーワード引数の場合はどんなキーワードを指定してもエラーにならない
+# 無効なキー(ここではchicken)をエラーにするためにはメソッド側で明示的な実装が必要
+buy_burger('big', potato: true, drink: true, chicken: false)
